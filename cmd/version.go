a6e29589f1f986904694d210660db37597c6cc81
Helvio Junior <helvio_junior@hotmail.com>
2025-07-21 11:54:56 -0300

    Release version only

--- cmd/version.go
+++ cmd/version.go
@@ -8,20 +8,28 @@ import (
     "github.com/spf13/cobra"
 )
 
+var releaseOnly = false
 var versionCmd = &cobra.Command{
     Use:   "version",
     Short: "Get the intelparser version",
     Long:  ascii.LogoHelp(`Get the intelparser version.`),
     Run: func(cmd *cobra.Command, args []string) {
-        fmt.Println(ascii.Logo())
+        if releaseOnly {
+            fmt.Printf("%s\n",
+                version.Version)
+        }else{
+            fmt.Println(ascii.Logo())
 
-        fmt.Println("Author: Helvio Junior (m4v3r1ck)")
-        fmt.Println("Source: https://github.com/helviojunior/intelparser")
-        fmt.Printf("Version: %s\nGit hash: %s\nBuild env: %s\nBuild time: %s\n\n",
-            version.Version, version.GitHash, version.GoBuildEnv, version.GoBuildTime)
+            fmt.Println("Author: Helvio Junior (m4v3r1ck)")
+            fmt.Println("Source: https://github.com/helviojunior/intelparser")
+            fmt.Printf("Version: %s\nGit hash: %s\nBuild env: %s\nBuild time: %s\n\n",
+                version.Version, version.GitHash, version.GoBuildEnv, version.GoBuildTime)
+        }
     },
 }
 
 func init() {
     rootCmd.AddCommand(versionCmd)
+
+    versionCmd.PersistentFlags().BoolVarP(&releaseOnly, "--release", "r", false, "Show release only")
 }
